6a322e37a3ebb591f3a2f022aa734bf8b74970fd
Joaquin Lerer <joacolerer@fi.uba.ar>
2021-02-09 13:10:29 -0300

    Agrego prueba de BloqueRepeticion

--- src/main/test/edu/fiuba/algo3/modelo/entrega2/Entrega2Test.java
+++ src/main/test/edu/fiuba/algo3/modelo/entrega2/Entrega2Test.java
@@ -13,9 +13,9 @@ class Entrega2Test {
 
     @Test
     public void laPizarraSeCreaCorrectamente(){
-        Pizarra pizzarra = new Pizarra();
+        Tablero tablero = new Tablero();
         HashSet<String> pizarraFinal = new HashSet<>();
-        assertEquals(pizzarra,pizarraFinal);
+        assertTrue(tablero.compararPizarra(pizarraFinal));
     }
 
     @Test
@@ -36,25 +36,46 @@ class Entrega2Test {
         tablero.agregarBloqueAlAlgoritmo(new BloqueDerecha());
         tablero.ejecutarAlgoritmo();
         HashSet<String> pizarraFinal = new HashSet<>();
-        Posicion pos1 = new Posicion(0,0);
-        pizarraFinal.add(pos1.ClaveString());
-        Posicion pos2 = new Posicion(0,1);
-        pizarraFinal.add(pos2.ClaveString());
-        Posicion pos3 = new Posicion(1,1);
-        pizarraFinal.add(pos3.ClaveString());
+        pizarraFinal.add(new Posicion(0,0).ClaveString());
+        pizarraFinal.add(new Posicion(0,1).ClaveString());
+        pizarraFinal.add(new Posicion(1,1).ClaveString());
         assertTrue(tablero.compararPizarra(pizarraFinal));
     }
 
     @Test
-    void crearAlgoritmoConBloqueRepeticion(){
+    void crearAlgoritmoConBloqueRepeticion2veces(){
         Tablero tablero = new Tablero();
         BloqueLapizApoyado bloqueApoyarLapiz = new BloqueLapizApoyado();
         BloqueRepetirN bloqueRepeticion = new BloqueRepetirN(2);
+        bloqueRepeticion.agregarBloque(new BloqueDerecha());
+        tablero.agregarBloqueAlAlgoritmo(bloqueApoyarLapiz);
+        tablero.agregarBloqueAlAlgoritmo(bloqueRepeticion);
+        tablero.ejecutarAlgoritmo();
+        HashSet<String> pizarraFinal = new HashSet<>();
+        pizarraFinal.add(new Posicion(0,0).ClaveString());
+        pizarraFinal.add(new Posicion(1,0).ClaveString());
+        pizarraFinal.add(new Posicion(2,0).ClaveString());
+        assertTrue(tablero.compararPizarra(pizarraFinal));
+    }
+    @Test
+    void crearAlgoritmoConBloqueRepeticion3veces(){
+        Tablero tablero = new Tablero();
+        BloqueLapizApoyado bloqueApoyarLapiz = new BloqueLapizApoyado();
+        BloqueRepetirN bloqueRepeticion = new BloqueRepetirN(3);
         bloqueRepeticion.agregarBloque(new BloqueAbajo());
         bloqueRepeticion.agregarBloque(new BloqueDerecha());
         tablero.agregarBloqueAlAlgoritmo(bloqueApoyarLapiz);
         tablero.agregarBloqueAlAlgoritmo(bloqueRepeticion);
         tablero.ejecutarAlgoritmo();
+        HashSet<String> pizarraFinal = new HashSet<>();
+        pizarraFinal.add(new Posicion(0,0).ClaveString());
+        pizarraFinal.add(new Posicion(0,1).ClaveString());
+        pizarraFinal.add(new Posicion(1,1).ClaveString());
+        pizarraFinal.add(new Posicion(1,2).ClaveString());
+        pizarraFinal.add(new Posicion(2,2).ClaveString());
+        pizarraFinal.add(new Posicion(2,3).ClaveString());
+        pizarraFinal.add(new Posicion(3,3).ClaveString());
+        assertTrue(tablero.compararPizarra(pizarraFinal));
     }
 
 }
