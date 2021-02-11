5da6e0d7e14db453118fd4ca1c30bc8c8cb4907d
ncrypto <npodesta@fi.uba.ar>
2021-02-11 20:05:07 -0300

    Cambio nombre atributo en Pizarra

--- src/main/java/edu/fiuba/algo3/modelo/Pizarra.java
+++ src/main/java/edu/fiuba/algo3/modelo/Pizarra.java
@@ -5,25 +5,25 @@ import java.util.Objects;
 
 public class Pizarra {
 
-    private final HashSet<String> pizarra; //cambiar nombre del atributo
+    private final HashSet<String> posicionesPintadas;
 
     @Override
     public boolean equals(Object unaPizarra) {
         if (this == unaPizarra) return true;
         HashSet<String> pizarraFinal = (HashSet<String>) unaPizarra;
-        return pizarra.equals(pizarraFinal);
+        return posicionesPintadas.equals(pizarraFinal);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(pizarra);
+        return Objects.hash(posicionesPintadas);
     }
 
     public Pizarra() {
-        pizarra = new HashSet<>();
+        posicionesPintadas = new HashSet<>();
     }
 
     public void pintarPosicion(Posicion posicion){
-        pizarra.add(posicion.ClaveString());
+        posicionesPintadas.add(posicion.ClaveString());
     }
 }
