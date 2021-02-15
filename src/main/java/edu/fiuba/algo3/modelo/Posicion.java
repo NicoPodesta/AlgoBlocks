addcc03fc26c27ccac78e07c22718b9b8094338c
DanteReinaudo <dantereinaudo@hotmail.com>
2021-02-15 20:32:00 -0300

    Test: se agregaron pruebas unitarias de los Bloques de movimiento y se elimino un atributo obsoleto de Posicion

--- src/main/java/edu/fiuba/algo3/modelo/Posicion.java
+++ src/main/java/edu/fiuba/algo3/modelo/Posicion.java
@@ -4,7 +4,6 @@ import java.util.Objects;
 
 public class Posicion {
 
-    private static final int LARGO = 5;
     private int x;
     private int y;
 
---
+++ src/main/test/edu/fiuba/algo3/modelo/PruebasUnitarias/BloqueDerechaTest.java
@@ -0,0 +1,29 @@
+package edu.fiuba.algo3.modelo.PruebasUnitarias;
+
+import edu.fiuba.algo3.modelo.BloqueDerecha;
+import edu.fiuba.algo3.modelo.Personaje;
+import edu.fiuba.algo3.modelo.Posicion;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BloqueDerechaTest {
+
+    @Test
+    public void ejecutarElBloqueMueveHaciaLaDerechaAlPersonaje(){
+        BloqueDerecha bloque = new BloqueDerecha();
+        Personaje personaje = new Personaje();
+        bloque.ejecutar(personaje);
+        Posicion coordenadas = new Posicion(1,0);
+        assertEquals(personaje.posicionActual(), coordenadas);
+    }
+
+    @Test
+    public void ejecutarInvertidoElBloqueMueveHaciaLaIzquierdaAlPersonaje(){
+        BloqueDerecha bloque = new BloqueDerecha();
+        Personaje personaje = new Personaje();
+        bloque.ejecutarInvertido(personaje);
+        Posicion coordenadas = new Posicion(-1,0);
+        assertEquals(personaje.posicionActual(), coordenadas);
+    }
+}
