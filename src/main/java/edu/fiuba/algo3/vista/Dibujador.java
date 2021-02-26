b85ed737ce10212d27dbede65d79078218bea6eb
Joaquin Lerer <joacolerer@fi.uba.ar>
2021-02-26 19:11:05 -0300

    Objeto Dibujador inicializando y dibujando correctamente

--- src/main/java/edu/fiuba/algo3/vista/Dibujador.java
+++ src/main/java/edu/fiuba/algo3/vista/Dibujador.java
@@ -1,30 +1,48 @@
 package edu.fiuba.algo3.vista;
 
 import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.LineTo;
 import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
 
 public class Dibujador {
-    public final int n = 10; //Numero de posibles segmentos
-    public final int TAM_LINEA = 75; //Tamaño de cada linea
-    private Group grupo;
-    private Path recorrido;
+    private final int n = 10; //Numero de posibles segmentos
+    private final int TAM_LINEA = 75; //Tamaño de cada linea
+    private final int posInicialRecorrido = 25;
 
+    private Path recorrido;
+    private Pane root;
+    private Scene scene;
     private double x_anterior;
     private double y_anterior;
 
-    public Dibujador(Path recorrido, int posInicialRecorrido){
+
+    public Dibujador(){
         //Group grupo = new Group(path); Ejemplos de google dicen que hay que agregar el recorrido a un objeto de tipo grupo pero no veo cambio alguno, lo dejo por las dudas
-        this.recorrido = recorrido;
+        this.recorrido = new Path();
+        this.root = new Pane(this.recorrido);
+        this.root.setPrefSize(n * TAM_LINEA, n * TAM_LINEA);
         this.recorrido.setStrokeWidth(3.0);
+        inicializarRecorrido();
+        this.scene  = new Scene(root, Color.WHITE);
+    }
+
+    private void inicializarRecorrido(){
         this.recorrido.getElements().add(new MoveTo(posInicialRecorrido, posInicialRecorrido)); //Posicion inicial del recorrido
-        actualizarXeYPrevios();
+        x_anterior = posInicialRecorrido;
+        y_anterior = posInicialRecorrido;
     }
 
     private void actualizarXeYPrevios(){
         LineTo lineaPrevia = (LineTo) this.recorrido.getElements().get(this.recorrido.getElements().size() - 1);
         x_anterior = lineaPrevia.getX();
         y_anterior = lineaPrevia.getY();
+    }
+
+    public Scene getEscena(){
+        return scene;
     }
 }
--- src/main/java/edu/fiuba/algo3/vista/PruebaVisualPizarra.java
+++ src/main/java/edu/fiuba/algo3/vista/PruebaVisualPizarra.java
@@ -14,37 +14,8 @@ public class PruebaVisualPizarra  extends Application {
     public final int offset = 25;
     @Override
     public void start(Stage stage)  {
-        //Creating a Path
-        Path path = new Path();
-        //Moving to the starting point
-        MoveTo moveTo = new MoveTo(offset, offset);
-        //Creating 1st line
-        LineTo line1 = new LineTo(offset + TAM_LINEA, offset);
-
-
-        //Adding all the elements to the path
-        path.getElements().add(moveTo);
-        path.getElements().addAll(line1);
-
-
-        LineTo lineaPrevia = (LineTo) path.getElements().get(path.getElements().size() - 1);
-        LineTo line2 = new LineTo(lineaPrevia.getX(), lineaPrevia.getY() + TAM_LINEA);
-        path.getElements().add(line2);
-
-        lineaPrevia = (LineTo) path.getElements().get(path.getElements().size() - 1);
-        LineTo line3 = new LineTo(lineaPrevia.getX() + TAM_LINEA, lineaPrevia.getY());
-        path.getElements().add(line3);
-
-        path.setStrokeWidth(3.0);
-        //Creating a Group object
-
-
-        Pane root = new Pane(path);
-        root.setPrefSize(n * TAM_LINEA, n * TAM_LINEA);
-
-        Scene scene = new Scene(root, Color.WHITE);
-
-        stage.setScene(scene);
+        Dibujador dibujador = new Dibujador();
+        stage.setScene(dibujador.getEscena());
         stage.setTitle("Pizarra");
         stage.show();
     }
