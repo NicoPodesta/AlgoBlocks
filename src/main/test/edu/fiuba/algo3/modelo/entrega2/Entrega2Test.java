55e5dd30c008d148043ca457c5ef2079ed7b2f60
DanteReinaudo <dantereinaudo@hotmail.com>
2021-03-03 00:44:47 -0300

    Test: se corrigen las pruebas de la segunda entrega

--- src/main/test/edu/fiuba/algo3/modelo/entrega2/Entrega2Test.java
+++ src/main/test/edu/fiuba/algo3/modelo/entrega2/Entrega2Test.java
@@ -1,71 +1,91 @@
 package edu.fiuba.algo3.modelo.entrega2;
 
 import edu.fiuba.algo3.modelo.*;
+import jdk.jshell.PersistentSnippet;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Entrega2Test {
 
     @Test
     public void laPizarraSeCreaCorrectamente(){
-        Tablero tablero = new Tablero();
+        ConjuntoBloques algoritmo = new ConjuntoBloques();
+        Personaje personaje = new Personaje();
         Pizarra pizarra = new Pizarra();
-        assertEquals(tablero.ejecutarAlgoritmo(),pizarra);
+        assertEquals(algoritmo.ejecutar(personaje),pizarra);
     }
 
     @Test
     public void testMoverElPersonajeConElLapizLevantadoNoPintaLaPizarra(){
-        Tablero tablero = new Tablero();
-        tablero.agregarBloqueAlAlgoritmo(new BloqueAbajo());
-        tablero.agregarBloqueAlAlgoritmo(new BloqueDerecha());
+        ConjuntoBloques algoritmo = new ConjuntoBloques();
+        Personaje personaje = new Personaje();
+        algoritmo.agregarBloque(new BloqueAbajo());
+        algoritmo.agregarBloque(new BloqueDerecha());
         Pizarra pizarra = new Pizarra();
-        assertEquals(tablero.ejecutarAlgoritmo(),pizarra);
+        assertEquals(algoritmo.ejecutar(personaje),pizarra);
     }
 
     @Test
     public void testMoverElPersonajeConElLapizAbajoPintaLaPizarra(){
-        Tablero tablero = new Tablero();
-        tablero.agregarBloqueAlAlgoritmo(new BloqueLapizApoyado());
-        tablero.agregarBloqueAlAlgoritmo(new BloqueAbajo());
-        tablero.agregarBloqueAlAlgoritmo(new BloqueDerecha());
+        ConjuntoBloques algoritmo = new ConjuntoBloques();
+        Personaje personaje = new Personaje();
+        algoritmo.agregarBloque(new BloqueLapizApoyado());
+        algoritmo.agregarBloque(new BloqueAbajo());
+        algoritmo.agregarBloque(new BloqueDerecha());
         Pizarra pizarra = new Pizarra();
-        pizarra.pintarTrazo(new Posicion(0,0));
-        pizarra.pintarTrazo(new Posicion(0,1));
-        pizarra.pintarTrazo(new Posicion(1,1));
-        assertEquals(tablero.ejecutarAlgoritmo(),pizarra);
+        Trazo trazo1 = new Trazo(new Posicion(0,0),new Posicion(0,1));
+        Trazo trazo2 = new Trazo(new Posicion(0,1),new Posicion(1,1));
+        pizarra.pintarTrazo(trazo1);
+        pizarra.pintarTrazo(trazo2);
+        assertEquals(algoritmo.ejecutar(personaje),pizarra);
     }
 
     @Test
     public void crearAlgoritmoConBloqueRepeticion2veces(){
-        Tablero tablero = new Tablero();
-        BloqueLapizApoyado bloqueApoyarLapiz = new BloqueLapizApoyado();
+        ConjuntoBloques algoritmo = new ConjuntoBloques();
+        Personaje personaje = new Personaje();
         BloqueRepetir bloqueRepeticion = new BloqueRepetir(2);
         bloqueRepeticion.agregarBloque(new BloqueDerecha());
-        tablero.agregarBloqueAlAlgoritmo(bloqueApoyarLapiz);
-        tablero.agregarBloqueAlAlgoritmo(bloqueRepeticion);
+        algoritmo.agregarBloque(new BloqueLapizApoyado());
+        algoritmo.agregarBloque(bloqueRepeticion);
+
         Pizarra pizarra = new Pizarra();
-        pizarra.pintarTrazo(new Posicion(0,0));
-        pizarra.pintarTrazo(new Posicion(1,0));
-        pizarra.pintarTrazo(new Posicion(2,0));
-        assertEquals(tablero.ejecutarAlgoritmo(),pizarra);
+        Trazo trazo1 = new Trazo(new Posicion(0,0),new Posicion(1,0));
+        Trazo trazo2 = new Trazo(new Posicion(1,0),new Posicion(2,0));
+        pizarra.pintarTrazo(trazo1);
+        pizarra.pintarTrazo(trazo2);
+
+        assertEquals(algoritmo.ejecutar(personaje),pizarra);
     }
 
     @Test
     public void crearAlgoritmoConBloqueRepeticion3veces(){
-        Tablero tablero = new Tablero();
+        ConjuntoBloques algoritmo = new ConjuntoBloques();
+        Personaje personaje = new Personaje();
+
         BloqueLapizApoyado bloqueApoyarLapiz = new BloqueLapizApoyado();
         BloqueRepetir bloqueRepeticion = new BloqueRepetir(3);
         bloqueRepeticion.agregarBloque(new BloqueAbajo());
         bloqueRepeticion.agregarBloque(new BloqueDerecha());
-        tablero.agregarBloqueAlAlgoritmo(bloqueApoyarLapiz);
-        tablero.agregarBloqueAlAlgoritmo(bloqueRepeticion);
+        algoritmo.agregarBloque(bloqueApoyarLapiz);
+        algoritmo.agregarBloque(bloqueRepeticion);
+
         Pizarra pizarra = new Pizarra();
-        pizarra.pintarTrazo(new Posicion(0,0));
-        pizarra.pintarTrazo(new Posicion(0,1));
-        pizarra.pintarTrazo(new Posicion(1,1));
-        pizarra.pintarTrazo(new Posicion(1,2));
-        pizarra.pintarTrazo(new Posicion(2,2));
-        pizarra.pintarTrazo(new Posicion(2,3));
-        pizarra.pintarTrazo(new Posicion(3,3));
-        assertEquals(tablero.ejecutarAlgoritmo(),pizarra);
+        Trazo trazo1 = new Trazo(new Posicion(0,0),new Posicion(0,1));
+        Trazo trazo2 = new Trazo(new Posicion(0,1),new Posicion(1,1));
+        Trazo trazo3 = new Trazo(new Posicion(1,1),new Posicion(1,2));
+        Trazo trazo4 = new Trazo(new Posicion(1,2),new Posicion(2,2));
+        Trazo trazo5 = new Trazo(new Posicion(2,2),new Posicion(2,3));
+        Trazo trazo6 = new Trazo(new Posicion(2,3),new Posicion(3,3));
+        pizarra.pintarTrazo(trazo1);
+        pizarra.pintarTrazo(trazo2);
+        pizarra.pintarTrazo(trazo3);
+        pizarra.pintarTrazo(trazo4);
+        pizarra.pintarTrazo(trazo5);
+        pizarra.pintarTrazo(trazo6);
+
+        assertEquals(algoritmo.ejecutar(personaje),pizarra);
     }
+
 }
