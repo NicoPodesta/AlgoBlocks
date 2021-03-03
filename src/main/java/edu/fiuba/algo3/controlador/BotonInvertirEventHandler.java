94d58bc020514e6555ac72f8cd2a84ec62132067
ncrypto <npodesta@fi.uba.ar>
2021-03-03 02:21:43 -0300

    Implemento otros handlers (basico)

--- src/main/java/edu/fiuba/algo3/controlador/BotonInvertirEventHandler.java
+++ src/main/java/edu/fiuba/algo3/controlador/BotonInvertirEventHandler.java
@@ -1,8 +1,13 @@
 package edu.fiuba.algo3.controlador;
 
+import edu.fiuba.algo3.modelo.BloqueInvertir;
+import edu.fiuba.algo3.modelo.BloqueRepetir;
 import edu.fiuba.algo3.modelo.ConjuntoBloques;
+import edu.fiuba.algo3.vista.ContenedorConjuntoBloques;
 import edu.fiuba.algo3.vista.ContenedorPrincipal;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class BotonInvertirEventHandler extends BotonBloqueEventHandler {
 
@@ -12,6 +17,9 @@ public class BotonInvertirEventHandler extends BotonBloqueEventHandler {
 
     @Override
     public void handle(MouseEvent event) {
-        //abrir ventana para generar conjuntos bloques
+        BloqueInvertir bloqueinvertir = new BloqueInvertir();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(new ContenedorConjuntoBloques(algoritmo, bloqueinvertir),1024,668));
+        stage.show();
     }
 }
