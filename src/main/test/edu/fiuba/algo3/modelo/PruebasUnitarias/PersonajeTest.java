1ce38c500ca683d784d0b5c9ca5ed6df6e1683ba
DanteReinaudo <dantereinaudo@hotmail.com>
2021-03-04 15:04:00 -0300

    Test: Se agregan pruebas a las pruebas unitarias de Personaje

--- src/main/test/edu/fiuba/algo3/modelo/PruebasUnitarias/PersonajeTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/PruebasUnitarias/PersonajeTest.java
@@ -1,7 +1,9 @@
 package edu.fiuba.algo3.modelo.PruebasUnitarias;
 
 import edu.fiuba.algo3.modelo.Personaje;
+import edu.fiuba.algo3.modelo.Pizarra;
 import edu.fiuba.algo3.modelo.Posicion;
+import edu.fiuba.algo3.modelo.Trazo;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -39,5 +41,67 @@ public class PersonajeTest {
         Posicion coordenadas = new Posicion(0,1);
         assertEquals(personaje.posicionActual(), coordenadas);
     }
+
+    @Test
+    public void elPersonajeSeMueveConLapizLevantadoyNoPintaLaPizarra() {
+        Personaje personaje = new Personaje();
+        personaje.levantarLapiz();
+        personaje.moverHaciaLaDerecha();
+        Pizarra pizarra = new Pizarra();
+        assertEquals(personaje.obtenerPizarra(), pizarra);
+    }
+
+    @Test
+    public void elPersonajeSeMueveConLapizApoyadoyPintaLaPizarra() {
+        Personaje personaje = new Personaje();
+        personaje.apoyarLapiz();
+        personaje.moverHaciaLaDerecha();
+        Pizarra pizarra = new Pizarra();
+        pizarra.pintarTrazo(new Trazo(new Posicion(0,0),new Posicion(1,0)));
+        assertEquals(personaje.obtenerPizarra(), pizarra);
+    }
+
+    @Test
+    public void elPersonajeSeIncializaConPizarraVacia() {
+        Personaje personaje = new Personaje();
+        Pizarra pizarra = new Pizarra();
+        assertEquals(personaje.obtenerPizarra(), pizarra);
+    }
+
+    @Test
+    public void elPersonajeSeMueveCorrectamenteSobreEjeX() {
+        Personaje personaje = new Personaje();
+        personaje.moverHaciaLaDerecha();
+        assertEquals(personaje.getX(), 1);
+    }
+
+    @Test
+    public void elPersonajeSeMueveCorrectamenteSobreEjeY() {
+        Personaje personaje = new Personaje();
+        personaje.moverHaciaAbajo();
+        assertEquals(personaje.getY(), 1);
+    }
+
+    @Test
+    public void elPersonajeQueSeReiniciaVuelveAPosicionInicial() {
+        Personaje personaje = new Personaje();
+        personaje.moverHaciaAbajo();
+        personaje.moverHaciaLaDerecha();
+        personaje.reiniciar();
+        Posicion posicion = new Posicion(0,0);
+        assertEquals(personaje.posicionActual(), posicion);
+    }
+
+    @Test
+    public void elPersonajeQueSeReiniciaBorraSuPizarra() {
+        Personaje personaje = new Personaje();
+        personaje.apoyarLapiz();
+        personaje.moverHaciaAbajo();
+        personaje.moverHaciaLaDerecha();
+        personaje.reiniciar();
+        Pizarra pizarra = new Pizarra();
+        assertEquals(personaje.obtenerPizarra(), pizarra);
+    }
+
 
 }
