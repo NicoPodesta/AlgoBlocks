723968981192ebf101fbe256b33cc038be558c65
ncrypto <npodesta@fi.uba.ar>
2021-03-04 01:19:58 -0300

    Agrego Textfield en BotonAgregarEventHandler

--- src/main/java/edu/fiuba/algo3/controlador/BotonAgregarEventHandler.java
+++ src/main/java/edu/fiuba/algo3/controlador/BotonAgregarEventHandler.java
@@ -3,6 +3,7 @@ package edu.fiuba.algo3.controlador;
 import edu.fiuba.algo3.modelo.BloquePersonalizado;
 import edu.fiuba.algo3.modelo.ConjuntoBloques;
 import javafx.event.EventHandler;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -12,20 +13,20 @@ public class BotonAgregarEventHandler implements EventHandler<MouseEvent> {
 
     private ConjuntoBloques algoritmo;
     private ArrayList<BloquePersonalizado> bloquesPersonalizados;
-    private String nombre;
+    private TextField texto;
     private Stage stage;
 
     public BotonAgregarEventHandler(ConjuntoBloques algoritmo, ArrayList<BloquePersonalizado> bloquesPersonalizados,
-                                    String nombre, Stage stage) {
+                                    TextField texto, Stage stage) {
         this.algoritmo = algoritmo;
         this.bloquesPersonalizados = bloquesPersonalizados;
-        this.nombre = nombre;
+        this.texto = texto;
         this.stage = stage;
     }
 
     @Override
     public void handle(MouseEvent event) {
-        BloquePersonalizado bloque = new BloquePersonalizado(nombre, algoritmo);
+        BloquePersonalizado bloque = new BloquePersonalizado(texto.getText(), algoritmo);
         bloquesPersonalizados.add(bloque);
         stage.close();
     }
