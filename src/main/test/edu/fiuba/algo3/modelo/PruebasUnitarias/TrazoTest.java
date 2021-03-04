9d9d26726aa46c74836db6d58b676be53abbfd34
DanteReinaudo <dantereinaudo@hotmail.com>
2021-03-04 13:01:40 -0300

    Test: se agregan mas pruebas a las pruebas unitarias del Trazo

--- src/main/test/edu/fiuba/algo3/modelo/PruebasUnitarias/TrazoTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/PruebasUnitarias/TrazoTest.java
@@ -4,7 +4,7 @@ import edu.fiuba.algo3.modelo.Posicion;
 import edu.fiuba.algo3.modelo.Trazo;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TrazoTest {
 
@@ -25,5 +25,30 @@ public class TrazoTest {
         Trazo trazo2 = new Trazo(coordenadas2,coordenadas1);
         assertEquals(trazo1,trazo2);
     }
+
+    @Test
+    public void laPrimeraPosicionDelTrazoSeGuardaCorrectamente() {
+        Posicion coordenadas1 = new Posicion(0,0);
+        Posicion coordenadas2 = new Posicion(0,1);
+        Trazo trazo = new Trazo(coordenadas1,coordenadas2);
+        assertEquals(coordenadas1,trazo.getPosicionA());
+    }
+
+    @Test
+    public void laSegundaPosicionDelTrazoSeGuardaCorrectamente() {
+        Posicion coordenadas1 = new Posicion(0,0);
+        Posicion coordenadas2 = new Posicion(0,1);
+        Trazo trazo = new Trazo(coordenadas1,coordenadas2);
+        assertEquals(coordenadas2,trazo.getPosicionB());
+    }
+
+    @Test
+    public void elTrazoNoEstaDesfasado() {
+        Posicion coordenadas1 = new Posicion(0,0);
+        Posicion coordenadas2 = new Posicion(0,1);
+        Trazo trazo = new Trazo(coordenadas1,coordenadas2);
+        assertFalse(trazo.estaDesfasado());
+    }
+
 
 }
