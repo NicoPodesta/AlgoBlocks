ef77ba5358596bbaab324bfad44b70f5bee4ecfd
DanteReinaudo <dantereinaudo@hotmail.com>
2021-03-04 13:01:29 -0300

    Test: se agregan mas pruebas a las pruebas unitarias de la Posicion

--- src/main/test/edu/fiuba/algo3/modelo/PruebasUnitarias/PosicionTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/PruebasUnitarias/PosicionTest.java
@@ -76,4 +76,57 @@ public class PosicionTest {
         assertEquals(posicion, nuevaPosicion);
     }
 
+
+    @Test
+    public void mePasoDelLimiteIzquierdoYCruzoAlOtroLado() {
+        Posicion posicion = new Posicion(0,0);
+        for (int i = 0; i < 6; ++i) {
+            posicion.izquierda();
+        }
+        Posicion nuevaPosicion = new Posicion(5,0);
+        assertEquals(posicion, nuevaPosicion);
+    }
+
+    @Test
+    public void mePasoDelLimiteDerechoYCruzoAlOtroLado() {
+        Posicion posicion = new Posicion(0,0);
+        for (int i = 0; i < 6; ++i) {
+            posicion.derecha();
+        }
+        Posicion nuevaPosicion = new Posicion(-5,0);
+        assertEquals(posicion, nuevaPosicion);
+    }
+
+    @Test
+    public void mePasoDelLimiteInferiorYCruzoAlOtroLado() {
+        Posicion posicion = new Posicion(0,0);
+        for (int i = 0; i < 6; ++i) {
+            posicion.abajo();
+        }
+        Posicion nuevaPosicion = new Posicion(0,-5);
+        assertEquals(posicion, nuevaPosicion);
+    }
+
+    @Test
+    public void mePasoDelLimiteSuperiorYCruzoAlOtroLado() {
+        Posicion posicion = new Posicion(0,0);
+        for (int i = 0; i < 6; ++i) {
+            posicion.arriba();
+        }
+        Posicion nuevaPosicion = new Posicion(0,5);
+        assertEquals(posicion, nuevaPosicion);
+    }
+
+    @Test
+    public void elEjeXSeGuardaCorrectamente() {
+        Posicion posicion = new Posicion(0,1);
+        assertEquals(0, posicion.getX());
+    }
+
+    @Test
+    public void elEjeYSeGuardaCorrectamente() {
+        Posicion posicion = new Posicion(2,1);
+        assertEquals(1, posicion.getY());
+    }
+
 }
