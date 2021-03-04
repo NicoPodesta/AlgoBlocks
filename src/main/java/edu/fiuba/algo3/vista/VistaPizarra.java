7bdd1fe453919e8913b5570746606d8f10e14c49
ncrypto <npodesta@fi.uba.ar>
2021-03-03 21:40:30 -0300

    Arreglo algunos bugs (no todos)

--- src/main/java/edu/fiuba/algo3/vista/VistaPizarra.java
+++ src/main/java/edu/fiuba/algo3/vista/VistaPizarra.java
@@ -11,6 +11,9 @@ import javafx.scene.shape.LineTo;
 import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
 
+import java.util.Timer;
+import java.util.concurrent.TimeUnit;
+
 public class VistaPizarra implements Observer {
 
     private final int n = 10; //Numero de posibles segmentos
@@ -20,21 +23,25 @@ public class VistaPizarra implements Observer {
 
     private Pizarra pizarra;
 
+    private ImageView playerView;
+
     public Path recorrido;
     public Pane root;
 
     public VistaPizarra(Pizarra pizarra){
-        //Group grupo = new Group(path); Ejemplos de google dicen que hay que agregar el recorrido a un objeto de tipo grupo pero no veo cambio alguno, lo dejo por las dudas
-        this.pizarra = pizarra;
-        this.recorrido = new Path();
-        this.root = new Pane(this.recorrido);
-        this.root.setPrefSize(n * TAM_LINEA, n * TAM_LINEA);
-        BackgroundImage miBackground= new BackgroundImage(new Image("file:src/main/java/edu/fiuba/algo3/vista/Imagenes/fondo_dibujador.png",n * TAM_LINEA,n * TAM_LINEA,false,false),
+        //Group grupo = new Group(path); Ejemplos de google dicen que hay que agregar el recorrido a un objeto de
+        // tipo grupo pero no veo cambio alguno, lo dejo por las dudas
+        pizarra = pizarra;
+        recorrido = new Path();
+        root = new Pane(this.recorrido);
+        root.setPrefSize(n * TAM_LINEA, n * TAM_LINEA);
+        BackgroundImage miBackground= new BackgroundImage(new Image("file:src/main/java/edu/fiuba/algo3/vista/Imagenes/fondo_dibujador.png",
+                n * TAM_LINEA,n * TAM_LINEA,false,false),
                 BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
                 BackgroundSize.DEFAULT);
-        this.root.setBackground(new Background(miBackground));
-        this.recorrido.setStrokeWidth(4.5);
-        this.root.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY,
+        root.setBackground(new Background(miBackground));
+        recorrido.setStrokeWidth(4.5);
+        root.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY,
                 new BorderWidths(2))));
     }
 
@@ -52,22 +59,25 @@ public class VistaPizarra implements Observer {
             int yPosB = posInicial + TAM_LINEA * trazo.getPosicionB().getY();
             recorrido.getElements().add(new MoveTo(xPosA, yPosA));
             dibujarCirculo(xPosA, yPosA);
-            this.recorrido.getElements().add(new LineTo(xPosB, yPosB));
+            recorrido.getElements().add(new LineTo(xPosB, yPosB));
             dibujarCirculo(xPosB, yPosB);
         }
     }
 
     public void clear() {
         recorrido.getElements().clear();
+        //root.getChildren().clear();
+        //root.getChildren().add(playerView);
     }
 
-    public void addImageView(ImageView imageView) {
-        root.getChildren().add(imageView);
+    public void setPlayerView(ImageView playerView) {
+        root.getChildren().add(playerView);
+        playerView = playerView;
     }
 
     private void dibujarCirculo(int x, int y){
         Circle circulo = new Circle(x, y,4);
         circulo.setFill(Color.BLACK);
-        this.root.getChildren().add(circulo);
+        root.getChildren().add(circulo);
     }
 }
